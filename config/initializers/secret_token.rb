ea284efec4606bbd7a0053c8f580f400b6f6b06b
len518 <rubio518@gmail.com>
2011-02-10 18:45:00 -0600

    despues de crear el modelo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab4::Application.config.secret_token = '0afe67a44e793fa7e24a909469e984b5ffa78d2f57eb496c6c86679d74ada54bde745e076c3bb1921d44636ce641a485cb1cb4f0e6724d9dcca11d7a2f7d1ff8'
